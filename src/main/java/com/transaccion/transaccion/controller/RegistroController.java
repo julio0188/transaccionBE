de3c4c28f3183a8f907da22d7455eb650f3c4d14
Julio Rodriguez <kl.julio@gmail.com>
2023-07-19 16:20:55 -0600

    first commit

--- src/main/java/com/transaccion/transaccion/controller/RegistroController.java
+++ src/main/java/com/transaccion/transaccion/controller/RegistroController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RestController
 @RequestMapping("/registro")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 public class RegistroController {
     @Autowired
     RegistroService registroService;
